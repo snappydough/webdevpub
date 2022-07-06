eb99daa768993392cdda216681bbc7aaaa0a8825
lol <94118385+snappydough@users.noreply.github.com>
2022-07-06 21:47:49 +0200

    Add files via upload

---
+++ videos/src/videos.js
@@ -0,0 +1,57 @@
+export const videos = [
+    {
+        "poster": "https://i.ytimg.com/vi/zEo3mgXkNFI/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLD1gqHsiFHWNcQTLHtaoU-d3i_Oow",
+        "src": "myvideo1.mp4"
+    },
+    {
+        "poster": "https://preview.redd.it/g8qohwjos2b81.jpg?auto=webp&s=0482770db8196f3be9d7c6ca486500bb16cc3585",
+        "src": "myvideo2.mp4"
+    },
+    {
+        "poster": "https://steamuserimages-a.akamaihd.net/ugc/1012688012793474957/44A6319AABFA22D10CDF4564FD08620238DE22CD/?imw=637&imh=358&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=true",
+        "src": "myvideo3.mp4"
+    },
+    {
+        "poster": "https://i.redd.it/eho9kji63qn71.jpg",
+        "src": "myvideo4.mp4"
+    },
+    {
+        "poster": "https://i.kym-cdn.com/entries/icons/original/000/040/002/cover2.jpg",
+        "src": "myvideo5.mp4"
+    },
+    {
+        "poster": "https://pbs.twimg.com/profile_images/1294466156577857537/Uglee9RE_400x400.jpg",
+        "src": "myvideo6.mp4"
+    },
+    {
+        "poster": "https://www.vadvivet.se/content/images/2020/09/1024px-Joe_Biden_official_portrait_2013.jpg",
+        "src": "myvideo7.mp4"
+    },
+    {
+        "poster": "https://i1.sndcdn.com/avatars-000195423408-ksba0p-t500x500.jpg",
+        "src": "myvideo8.mp4"
+    },
+    {
+        "poster": "https://i.ytimg.com/vi/zjoJkOj1wN4/maxresdefault.jpg",
+        "src": "myvideo9.mp4"
+    },
+    {
+        "poster": "https://i.pinimg.com/474x/df/93/d6/df93d60882fea9afbede856373a00b35.jpg",
+        "src": "myvideo10.mp4"
+    },
+    {
+        "poster": "https://i.ytimg.com/vi/9RQW8BWBTxA/maxresdefault.jpg",
+        "src": "myvideo11.mp4"
+    },
+    {
+        "poster": "https://www.billboard.com/wp-content/uploads/2021/01/video-9506651-image.jpg?w=681&h=383&crop=1",
+        "src": "myvideo12.mp4"
+    },
+    {
+        "poster": "https://thumbs.dreamstime.com/b/man-eating-sandwich-4327189.jpg",
+        "src": "myvideo13.mp4"
+    }
+    
+    
+    
+]
